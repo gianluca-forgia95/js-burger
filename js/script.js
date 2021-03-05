0d7c6f1ce4dbfbc04d0d0d610581f3d2a023687e
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-05 15:08:48 +0100

    stampo la somma in console

--- js/script.js
+++ js/script.js
@@ -4,16 +4,29 @@ Il programma dovrà consentire di calcolare il prezzo del panino selezionando o 
 */
 
 //Creo variabili
+//Definisco variabile per il bottone di calcolo
 var calc = document.getElementById('button-calc');
-console.log(calc);
+//Definisco variabile per il nome dell'hamburger
 var burgerName = document.getElementById('burger-name');
-console.log(burgerName);
+//Definisco variabile per gli ingredienti del panino
+var ingredientiPanino = document.getElementsByClassName('ingredients');
+//Definisco variabile per il totale
+var total = document.getElementById('totale');
 
 calc.addEventListener("click",
 function() {
-  //Controllo se il nome è stato inserito
+  //Controllo se il nome è stato inserito e metto le condizioni
   if ( burgerName.value != "") {
-    console.log( burgerName.value );
+    //Definisco il ciclo degli ingredienti
+    var somma = 50;
+    for (var i = 0; i < ingredientiPanino.length; i++) {
+
+      if ( ingredientiPanino[i].checked == true ) {
+        somma = somma + parseInt( ingredientiPanino[i].value );
+      }
+
+    }
+      console.log(somma);
   } else {
     alert('Inserisci il nome del panino');
   }
